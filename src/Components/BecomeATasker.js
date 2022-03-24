b3b49ff55bf332e6110739bfccda482c35fa3b04
SalmaaOsamaa <salma.usama1996@gmail.com>
2022-03-24 14:38:52 +0200

    done

--- src/Components/BecomeATasker.js
+++ src/Components/BecomeATasker.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import img from '../assets/images/mounting.jpg'
+import img from '../assets'
 
 function BecomeATasker() {
   return (
